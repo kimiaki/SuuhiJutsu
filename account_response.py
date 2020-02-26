d42cbdee7e010e77588fc0dda5260687fae6c070
kimiaki <flyaway-to-the-sky@hotmail.co.jp>
2020-02-26 13:38:48 +0900

    upload class python

---
+++ account_response.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+class Response:
+    dic={"会社概要":"COMPANY",
+         "事業内容":"SERVICE",
+         "採用情報":"CAREER",
+         "コマンド":"COMMAND",
+         "おはよう":"5",
+         "こんにちは":"5",
+         "こんばんは":"5",
+         "こんにちわ":"6",
+         "こんばんわ":"6"
+         }
+    count=0
+    
+    def getResponse(self,text):
+        if self.count >= 4:
+            self.count = 0
+        self.count += 1
+        for _dic in self.dic:
+            if _dic == text:
+                return self.dic[text]
+        return str(self.count)
